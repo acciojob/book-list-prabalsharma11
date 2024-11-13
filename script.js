63864ca705b271808c0a0224985478d7984d9587
Prabal Raj Sharma <109718530+prabalsharma11@users.noreply.github.com>
2024-11-13 23:36:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+const  booklist = document.getElementById("book-list")
+function sub(){
+	const et = document.getElementById("title").value.trim();
+	const at = document.getElementById("author").value.trim();
+	const it = document.getElementById("isbn").value.trim();
+	if(!et||!at||!it){
+		alert("entr vlue")
+	}
+	const row = document.createElement("tr")
+		row.innerHTML = `
+		<td>${et}</td>
+		<td>${at}</td>
+		<td>${it}</td>
+		<td ><button class="delete" >clear</button></td>`
+	
+	booklist.appendChild(row);
+	document.getElementById("title").value= '';
+	document.getElementById("author").value= '';
+	document.getElementById("isbn").value= '';
+	
+	
+}
+bookList.addEventListener('click',(e){
+	if(e.target.classList.contains('delete')){
+		const row = e.target.parentElement.parentElement;
+		booklist.removeChild(row)
+	}
+})
